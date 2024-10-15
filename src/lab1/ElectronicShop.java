6b6eb3c29cef614f6f5ab8267c19e8d0a05f9a15
Matei_Dana <dana.matei@stud.ubbcluj.ro>
2024-10-16 00:41:38 +0300

    lab1 - aufgabe4

---
+++ src/lab1/ElectronicShop.java
@@ -0,0 +1,62 @@
+package lab1;
+
+import java.util.Arrays;
+
+public class ElectronicShop {
+
+    // find cheapest keyboard
+    public static int findCheapestKeyboard(int[] keyboards) {
+        int minPrice = Integer.MAX_VALUE;
+        for (int price : keyboards)
+            if (price < minPrice)
+                minPrice = price;
+        return minPrice;
+    }
+
+    // find the most expensive article
+    public static int findMostExpensiveItem(int[] keyboards, int[] usbDrives) {
+        int maxPrice = 0;
+        for (int price : keyboards)
+            if (price > maxPrice)
+                maxPrice = price;
+        for (int price : usbDrives)
+            if (price > maxPrice)
+                maxPrice = price;
+        return maxPrice;
+    }
+
+    // find the most expensive USB markus can buy
+    public static int findMostExpensiveUSB(int[] prices, int budget) {
+        int max_affordable_price = 0;
+        for (int price : prices) {
+            if (price <= budget && price > max_affordable_price) {
+                max_affordable_price = price;
+            }
+        }
+        return max_affordable_price;
+    }
+
+    // Metodă pentru a găsi suma maximă pe care Markus o poate cheltui
+    public static int maxSpendableAmount(int budget, int[] keyboards, int[] usbDrives) {
+        int maxSpend = -1;
+        for (int kb : keyboards) {
+            for (int usb : usbDrives) {
+                int total = kb + usb;
+                if (total <= budget && total > maxSpend) {
+                    maxSpend = total; // update max sum
+                }
+            }
+        }
+        return maxSpend;
+    }
+
+    public static void main(String[] args) {
+        int[] keyboards = {40, 35, 70, 15, 45};
+        int[] usbDrives = {20, 15, 40, 15};
+        int budget = 30;
+        System.out.println("Cheapest keyboard: " + findCheapestKeyboard(keyboards));
+        System.out.println("Most expensive article: " + findMostExpensiveItem(keyboards, usbDrives));
+        System.out.println("Most expensive USB he can buy: " + findMostExpensiveUSB(usbDrives, budget));
+        System.out.println("Max sum he can spend: " + maxSpendableAmount(budget, keyboards, usbDrives));
+    }
+}
